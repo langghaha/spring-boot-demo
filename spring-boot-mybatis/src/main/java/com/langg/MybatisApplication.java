9f069ed820c498f13f9cb8d51d0c07c65fcdeb04
langghaha <yczhuhaogg@gmail.com>
2019-11-06 16:37:00 +0800

    add spring-boot-jpa demo

---
+++ spring-boot-mybatis/src/main/java/com/langg/MybatisApplication.java
@@ -0,0 +1,19 @@
+package com.langg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author zh
+ * @date 2019/11/6 15:01
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class MybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisApplication.class, args);
+    }
+}
